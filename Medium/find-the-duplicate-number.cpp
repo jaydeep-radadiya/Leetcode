dd6ace74ce4da3a29118a37bde415eba12ac7f54
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-10-05 23:11:58 +0530

    leetcode solution

--- Medium/find-the-duplicate-number.cpp
+++ Medium/find-the-duplicate-number.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        unordered_set<int> ss;
+        for(int ele : nums){
+            int ps = ss.size();
+            ss.insert(ele);
+            int as = ss.size();
+            if(ps==as) return ele;
+        }
+        return -1;
+    }
+};
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow!=fast);
         
+        fast = nums[0];
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1])
+                return nums[i];
+        }
+        return -1;
     }
 };
