33ebe4595d57a94f615a21329fcd9d97361f9a6e
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-10-26 18:07:31 +0530

    inorder traversal

---
+++ Easy/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// tc : O(n) bcoz we traverse every node once
+// recursive
+class Solution {
+public:
+    void inorder(TreeNode *root,vector<int> &ans){
+        if(root==NULL) return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder(root,ans);
+        return ans;
+    }
+};
+
+// iterative
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==NULL) return {};
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode *curr = root;
+        
+        while(curr!=NULL or not st.empty()){
+            while(curr!=NULL){
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->val);
+            curr = curr->right;
+        }
+        return ans;
+    }
+};
