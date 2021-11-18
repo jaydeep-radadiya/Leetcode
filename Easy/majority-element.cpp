822ad5cdf19baf4854cd5b2be5225490dd45634a
Jaydeep Radadiya <jaydeepradadiya2000@gmail.com>
2021-11-18 13:10:23 +0530

    other solution added

--- Easy/majority-element.cpp
+++ Easy/majority-element.cpp
@@ -1,3 +1,56 @@
+// brute force solution 
+// tc : O(n^2) sc : O(1)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        
+        for(int i=0;i<n;i++){
+            int cnt = 0;
+            for(int j=0;j<n;j++){
+                if(nums[i]==nums[j]){
+                    cnt++;
+                }
+            }
+            if(cnt>(n/2)){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+// tc : O(n*logn) sc : O(1)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        return nums[n/2];
+    }
+};
+
+// tc : O(n) sc : O(n)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> ump;
+        
+        for(int i=0;i<n;i++)
+            ump[nums[i]]++;
+        
+        for(auto it : ump){
+            if(it.second>(n/2)){
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
+
+// moore's voting algorithm
+// tc : O(n) sc : O(1)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
